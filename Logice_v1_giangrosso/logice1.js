9e014c9801daa7b57f703c81317a0526daa7544a
Matt Giangrosso <103087661+mgiangr1@users.noreply.github.com>
2023-03-14 18:50:04 -0500

    Formatting
    Minor edits to formatting

--- Logice_v1_giangrosso/logice1.js
+++ Logice_v1_giangrosso/logice1.js
@@ -1,22 +1,22 @@
-const daysTag = document.querySelector(".days"),
-currentDate = document.querySelector(".current-date"),
-prevNextIcon = document.querySelectorAll(".icons span"),
-curDayIcon = document.querySelectorAll(".wrapper ul"); // creating days icons
+const daysTag = document.querySelector(".days");
+const currentDate = document.querySelector(".current-date");
+const prevNextIcon = document.querySelectorAll(".icons span");
+const curDayIcon = document.querySelectorAll(".wrapper ul"); // creating days icons
 // getting new date, current year and month
-let date = new Date(),
-currYear = date.getFullYear(),
-currMonth = date.getMonth();
+let date = new Date();
+let currYear = date.getFullYear();
+let currMonth = date.getMonth();
 // storing full name of all months in array
 const months = ["January", "February", "March", "April", "May", "June", "July",
               "August", "September", "October", "November", "December"];
 const renderCalendar = () => {
-    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
-    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
-    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
-    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(); // getting first day of month
+    let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(); // getting last date of month
+    let lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(); // getting last day of month
+    let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
     let liTag = "";
-    for (let k = firstDayofMonth; k > 0; k--) { // creating li of previous month last days
-        liTag += `<li class="inactive">${lastDateofLastMonth - k + 1}</li>`;
+    for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
+        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
     }
     for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
         // adding active class to li if the current day, month, and year matched
