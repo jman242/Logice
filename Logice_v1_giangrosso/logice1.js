a2edfd86920b9761451a5954073cf0d545a432a8
Matt Giangrosso <103087661+mgiangr1@users.noreply.github.com>
2023-03-15 21:37:31 -0500

    Added comments

--- Logice_v1_giangrosso/logice1.js
+++ Logice_v1_giangrosso/logice1.js
@@ -1,56 +1,65 @@
 const daysTag = document.querySelector(".days");
-const currentDate = document.querySelector(".current-date");
+const currentDate = document.querySelector(".current-date"); //month view date (Month and Year)
+const currentDate2 = document.querySelector(".current-date2"); //week view date (Month and Year)
 const prevNextIcon = document.querySelectorAll(".icons span");
-const curDayIcon = document.querySelectorAll(".wrapper ul"); // creating days icons
-// getting new date, current year and month
-let date = new Date();
+const curDayIcon = document.querySelectorAll(".wrapper ul"); //creating days icons
+const hoursTag = document.querySelector(".hours"); //creating hours for week view
+
+//creating new date and getting current year, month, and day
+let date = new Date(); 
 let currYear = date.getFullYear();
-let currMonth = date.getMonth();
-// storing full name of all months in array
+let currMonth = date.getMonth(); 
+let currDay = date.getDay(); 
+
+//storing full name of all months in array
 const months = ["January", "February", "March", "April", "May", "June", "July",
               "August", "September", "October", "November", "December"];
 const renderCalendar = () => {
-    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(); // getting first day of month
-    let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(); // getting last date of month
-    let lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(); // getting last day of month
-    let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(); //getting first day of month
+    let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(); //getting last date of month
+    let lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(); //getting last day of month
+    let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); //getting last date of previous month
     let liTag = "";
-    for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
+    for (let i = firstDayofMonth; i > 0; i--) { //creating li of previous month last days
         liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
     }
-    for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
-        // adding active class to li if the current day, month, and year matched
+    for (let i = 1; i <= lastDateofMonth; i++) { //creating li of all days of current month
+        //adding active class to li if the current day, month, and year matched
         let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
                      && currYear === new Date().getFullYear() ? "active" : "";
         liTag += `<li class="${isToday}">${i}</li>`;
     }
-    for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
+    for (let i = lastDayofMonth; i < 6; i++) { //creating li of next month first days
         liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
     }
-    currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
+    currentDate.innerText = `${months[currMonth]} ${currYear}`; //passing current mon and yr as currentDate text (Month View)
+    currentDate2.innerText = `${months[currMonth]} ${currYear}`; //passing current mon and yr as currentDate2 text (Week View)
     daysTag.innerHTML = liTag;
 }
 renderCalendar();
 
-prevNextIcon.forEach(icon => { // getting prev and next icons
-    icon.addEventListener("click", () => { // adding click event on both icons
-        // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+
+prevNextIcon.forEach(icon => { //getting prev and next icons
+    icon.addEventListener("click", () => { //adding click event on both icons
+        //if clicked icon is previous icon then decrement current month by 1 else increment it by 1
         currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
-        if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
-            // creating a new date of current year & month and pass it as date value
+        if(currMonth < 0 || currMonth > 11) { //if current month is less than 0 or greater than 11
+            //creating a new date of current year & month and pass it as date value
             date = new Date(currYear, currMonth, new Date().getDate());
-            currYear = date.getFullYear(); // updating current year with new date year
-            currMonth = date.getMonth(); // updating current month with new date month
+            currYear = date.getFullYear(); //updating current year with new date year
+            currMonth = date.getMonth(); //updating current month with new date month
         } else {
-            date = new Date(); // pass the current date as date value
+            date = new Date(); //pass the current date as date value
         }
-        renderCalendar(); // calling renderCalendar function
+        renderCalendar(); //calling renderCalendar function
     });
 
-//This function is being used to provide the user with the ability to click on any day of the month in the calendar and see a view of that day.
+
 curDayIcon.forEach(icon => { //getting current day icon
     icon.addEventListener("click", () => { //adding click event on current day icon
-        alert("you did it"); // Event will need to be added here in order to produce a day view for the user. 
+        //if clicked, the week of the current day will be displayed below the calendar
+        alert("Day view of this date should pop up here");
+        //Event will need to be added in this function to call a day view
     })
 });
 });
