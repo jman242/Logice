80a0767927b2dc52f51650b82c3f6facf92a0fc8
mgiangr1 <matt.giangrosso@ku.edu>
2023-04-05 15:56:18 -0500

    Added mock events to week view

--- Logice_v1_giangrosso/js/calendar.js
+++ Logice_v1_giangrosso/js/calendar.js
@@ -1,3 +1,5 @@
+import { calculateEventHeight } from "./utils.js";
+
 const daysTag = document.querySelector(".days");
 const currentDate = document.querySelector(".current-date"); //month view date (Month and Year)
 const currentDate2 = document.querySelector(".current-date2"); //week view date (Month and Year)
@@ -6,14 +8,15 @@ const curDayIcon = document.querySelectorAll(".wrapper ul"); //creating days ico
 const hoursTag = document.querySelector(".hours"); //creating hours for week view
 
 //creating new date and getting current year, month, and day
-let date = new Date(); 
+let date = new Date(); // date api
 let currYear = date.getFullYear();
-let currMonth = date.getMonth(); 
-let currDay = date.getDay(); 
+let currMonth = date.getMonth();
+let currDay = date.getDay();
 
 //storing full name of all months in array
 const months = ["January", "February", "March", "April", "May", "June", "July",
               "August", "September", "October", "November", "December"];
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const renderCalendar = (events) => {
     let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(); //getting first day of month
     let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(); //getting last date of month
@@ -34,26 +37,89 @@ const renderCalendar = (events) => {
         const date = i - lastDayofMonth + 1;
         liTag += `<li class="inactive" data-date-key="${`${months[currMonth + 1]}:${date}`}">${date}</li>`
     }
-    currentDate.innerText = `${months[currMonth]} ${currYear}`; //passing current mon and yr as currentDate text (Month View)
-    currentDate2.innerText = `${months[currMonth]} ${currYear}`; //passing current mon and yr as currentDate2 text (Week View)
+    currentDate.innerText = `${months[currMonth]} ${currYear}`; //passing current month and year as currentDate text (Month View)
+    currentDate2.innerText = `${months[currMonth]} ${currYear}`; //passing current month and year as currentDate2 text (Week View)
     daysTag.innerHTML = liTag;
 }
 renderCalendar();
 
-const renderTimeblocks = () => {
-    for(let i = 0; i < 7; i++ ) { // Loop for each day in a week
-        const currentDay = document.querySelector(`[data-day="${i}"] [data-time-block]`);
+const renderTimeblocks = (dateRef, events) => {
+    for(let i = 0; i < 7; i++ ) { // Loop for each day in a week and create timeblocks
+        const currentDay = document.querySelector(`[data-day="${i}"] [data-day-label]`);
+        const timeGrid = document.querySelector(`[data-day="${i}"] [data-time-block]`);
+        currentDay.innerHTML = `<span>${dateRef.week[i]}</span><span>${days[i]}</span>`
         for(let k = 0; k < 24; k++) { // Loop for each hour in a day
-            //const currentBlock = `<span class="hours">${k}:00</span>`
-            //currentDay.append(currentBlock);
             const spanElement = document.createElement("span");
+            spanElement.setAttribute("data-event-target", `${dateRef.month}:${dateRef.week[i]}:${k}`); 
             spanElement.classList.add("hours");
-            spanElement.innerText = `${k}:00`;
-            currentDay.appendChild(spanElement);
+            spanElement.innerHTML = `<span>${k}</span>`;
+            timeGrid.appendChild(spanElement);
         }
     }
+
+    events.forEach((event) =>{ // Attach events into timeblocks
+        let eventDate = new Date(event.starts_at); // date api
+        let month = eventDate.getMonth();
+        let day = eventDate.getDate();
+        let hour = eventDate.getHours() + 5; // + 5 for time offset adjustment
+        let minutes = eventDate.getMinutes();
+        let eventEndDate = new Date(event.ends_at)
+        let endHour = eventEndDate.getHours() + 5; // + 5 for time offset adjustment
+        let endMinutes = eventEndDate.getMinutes();
+
+        //let spanTag = "";
+
+        for(let j = 0; j < 12; j++){ //looping through months array to find correct month
+            if(months[j] == dateRef.month){
+                month = months[j]; //setting the month value to the correct month
+                break;
+            }
+        }
+        //if(// event.starts_at month matches dateRef.month){
+        if(month == dateRef.month){
+            for(let i = 0; i < 7; i++){ //adding loop to iterate through week array
+                // if(// event.starts_at day is present in dateRef.week array){
+                if(day == dateRef.week[i]){
+                    //build data-event-target key
+                    var eventTargetKey = `${month}:${day}:${hour}`;
+                    
+                    //console.log(month, day, hour);
+                    
+
+                    // look for element that matches key
+                    const eventTimeBlock = document.querySelector(`[data-event-target="${eventTargetKey}"]`); //attempting to grab Month:Day:Hour from html
+                    //console.log(eventTimeBlock.getAttribute("data-event-target"));
+                    //console.log(eventTargetKey);
+
+                    //spanTag = <span> data-event-target="`${month}:${day}:${hour}`"</span>;
+                    const spanElement = document.createElement("span");
+                    spanElement.innerHTML = `<span>${event.title}</span><span>${hour}:${minutes ? minutes : "00"} to ${endHour}:${endMinutes ? endMinutes : "00"}</span>` // ? if else statement ternary
+                    spanElement.style.backgroundColor="rgba(2, 4, 110, 0.571)"
+                    spanElement.style.height= `${calculateEventHeight(eventDate, eventEndDate)}px`; //calculating height based on difference 
+                    spanElement.style.marginTop= `${minutes}px`;
+                    spanElement.style.marginLeft="20px";
+                    eventTimeBlock.appendChild(spanElement);
+                }
+            }
+        }
+        
+/*
+        
+    //if(// event.starts_at month matches dateRef.month){ --
+
+    if(// event.starts_at day is present in dateRef.week array){ --
+
+      // build data-event-target key
+
+      // look for element that matches key
+
+     // create new element, add event details, then put inside element that matches key
+
+    }
+
+}*/
+    })
 }
-renderTimeblocks();
 
 prevNextIcon.forEach(icon => { //getting prev and next icons
     icon.addEventListener("click", () => { //adding click event on both icons
@@ -69,8 +135,8 @@ prevNextIcon.forEach(icon => { //getting prev and next icons
         }
         renderCalendar(); //calling renderCalendar function
     });
-
 });
+
 curDayIcon.forEach(icon => { //getting current day icon
     icon.addEventListener("click", (e) => { //adding click event on current day icon
         window.bus.publish("day:change", { key:e.target.dataset.dateKey });
@@ -79,6 +145,7 @@ curDayIcon.forEach(icon => { //getting current day icon
 });
 
 window.bus.subscribe("event:change", (payload) => {
-    console.log(payload.data);
-    renderCalendar(payload.data);
+    console.log(payload);
+    renderTimeblocks(payload.dateRef, payload.events); 
 })
+
