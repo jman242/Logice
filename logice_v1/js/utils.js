54cc44fd0ec4a5516dd36b84a71d70038051d5a2
mgiangr1 <matt.giangrosso@ku.edu>
2023-04-25 21:06:28 -0500

    updated file structure

--- logice_v1/js/utils.js
+++
@@ -1,4 +0,0 @@
-export function calculateEventHeight(starts_at, ends_at){ // exporting function for code consolidation
-    let diffMs = Math.abs(starts_at - ends_at); //getting the absolute value of miliseconds between start and end time
-    return Math.floor((diffMs/1000)/60); // dividing absolute value time difference by 1000 to get miliseconds and then by 60 for seconds in a a minute, then rounding down to floor and returning val
-}
