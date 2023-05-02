05f6c6ed758b0a72f4cfea45c76f82c484c2af8e
Tanner Fry <103395463+TannerFry-KiteAAI@users.noreply.github.com>
2023-05-02 13:53:10 -0500

    Update index.php
    Fixed login redirect

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if (isset($_SESSION['last_activity']) && time() - $_SESSION['last_activity'] > 9
   // last request was more than 15 minutes ago
   session_unset(); // unset $_SESSION variable for the run-time
   session_destroy(); // destroy session data in storage
-  header("Location: Login/login.html"); // redirect to login page
+  header("Location: login/index.php"); // redirect to login page
 }
 $_SESSION['last_activity'] = time(); // update last activity time stamp
 
