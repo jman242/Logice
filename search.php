c5de23d4b889ef3b7945ab1060c99b472191823d
jman242 <112523848+jman242@users.noreply.github.com>
2023-05-05 15:37:25 -0500

    Update search.php
    update for search.php to include header updates

--- search.php
+++ search.php
@@ -10,21 +10,22 @@ if (isset($_SESSION['userid']) && isset($_SESSION['username'])) {
 <html lang ="en" dir="ltr">
 <head>
     <meta charset="utf-8">
-    <title>Logice</title>
+    <title>Logice - Search</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://kit.fontawesome.com/e9fdb44b0e.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1$
 </head>
     <nav class="topline">
         <div class="container">
-            <i class="fa-regular fa-l"></i>
-            <h1>Logice User: <?php echo $_SESSION['username']; ?></h1>
-        <div class="topnav">
-            <a class="active" href="#Home">Home</a>
-            <a href="#todo">To Do</a>
-            <a href="#settings">Settings</a>
-            <a href="#logout">Logout</a>
+            <img class="headerImage" src="logoHeader.png" alt="Logice - Welcome:" height="53px" width="439px" />
+            <h1 class="headerh1"><?php echo $_SESSION['username']; ?></h1>
+            <div class="topnav">
+                <a class="active" href="index.php">Home</a>
+                <a href="notes/index.php">Notes</a>
+                <a href="search.php">Search Event</a>
+                <a href="login/logout.php">Logout</a>
+            </div>
         </div>
     </nav>
     <body>
